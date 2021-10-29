fc425d8cbecd2487ecc34a1b7c213733aa455804
Alejandro <amogalla@iesrafaelalberti.es>
2021-10-29 20:35:51 +0200

    Creada clase Persona con constructores y la función calcularIMC.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,35 @@
+var HOMBRE = 'H'
+
+class Persona(private val dni:String){
+    protected var nombre:String = ""
+    protected var edad:Int = 0
+    protected var sexo:Char = HOMBRE
+    protected var peso:Double = 0.0
+    protected var altura:Double = 0.0
+
+    constructor(dni:String, nombre:String, edad:Int, sexo:Char): this(dni){
+        this.nombre = nombre
+        this.edad = edad
+        this.sexo = sexo
+    }
+
+    constructor(dni:String, nombre:String, edad:Int, sexo:Char, peso:Double, altura:Double): this(dni, nombre, edad, sexo){
+        this.peso = peso
+        this.altura = altura
+    }
+
+
+    fun calcularIMC(): Int {
+        return when (peso/(altura*altura)){
+            in 1.0..20.0 -> -1
+            in 20.0..25.0 -> 0
+            else -> 1
+        }
+
+    }
+}
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
