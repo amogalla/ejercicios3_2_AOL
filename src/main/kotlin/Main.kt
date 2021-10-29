cb4ac9a0c61320e7ddfffbc06a24815a6c26aaac
Alejandro <amogalla@iesrafaelalberti.es>
2021-10-29 22:15:24 +0200

    Añadidas funciones para pedir edad, peso, altura y sexo.
    Creada función main para instanciar la clase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,23 @@
-var HOMBRE = 'H'
-var MUJER = 'M'
+class Persona(val dni:String) {
+    var nombre: String = ""
+    var edad: Int = 0
+    var sexo: Char = HOMBRE
+        set(value) {
+            field = comprobarSexo(value)
+        }
+    var peso: Double = 0.0
+    var altura: Double = 0.0
 
-class Persona(private val dni:String){
-    protected var nombre:String = ""
-    protected var edad:Int = 0
-    protected var sexo:Char = HOMBRE
-    protected var peso:Double = 0.0
-    protected var altura:Double = 0.0
+    companion object {
+        const val HOMBRE = 'H'
+        const val MUJER = 'M'
+    }
+
 
     constructor(dni:String, nombre:String, edad:Int, sexo:Char): this(dni){
         this.nombre = nombre
         this.edad = edad
-        this.sexo = comprobarSexo(sexo)
+        this.sexo = sexo
     }
 
     constructor(dni:String, nombre:String, edad:Int, sexo:Char, peso:Double, altura:Double): this(dni, nombre, edad, sexo){
@@ -38,12 +44,59 @@ class Persona(private val dni:String){
     }
 
     override fun toString() = "Nombre = $nombre, edad = $edad, DNI = $dni, sexo = $sexo, peso = $peso, altura = $altura"
+
+
+}
+
+fun generaLetraDNI(numeroDNI:Int): Char {
+    val letra:String = "TRWAGMYFPDXBNJZSQVHLCKE"
+    val resto:Int = numeroDNI%23
+    return letra.get(resto)
+}
+
+fun generaNumerosDNI():Int{
+    var numero = 0
+    for(i in 1..8)
+        numero = numero*10 + (0..10).random()
+    return numero
+}
+
+fun generarDNI():String{
+    val numero = generaNumerosDNI()
+    return numero.toString().plus(generaLetraDNI(numero).toString())
+}
+
+fun pedirNombre():String{
+    print("Introduzca el nombre: ")
+    return readLine()?:""
+}
+
+fun pedirEdad():Int {
+    print("Introduzca la edad: ")
+    return (readLine() ?: "0").toInt()
+}
+
+fun pedirPeso():Double {
+    print("Introduzca el peso: ")
+    return (readLine() ?: "0").toDouble()
+}
+
+fun pedirAltura():Double {
+    print("Introduzca la altura: ")
+    return (readLine() ?: "0").toDouble()
+}
+
+fun pedirSexo():Char {
+    print("Introduzca el sexo: ")
+    return (readLine()?: "H")[0]
 }
 
 
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val persona1 = Persona(generarDNI(), pedirNombre(), pedirEdad(), pedirSexo(), pedirPeso(), pedirAltura())
+    println(persona1.sexo)
+    val persona2 = Persona(generarDNI(), "Sandra", 27, 'M')
+    println(persona2.dni)
+    val persona3 = Persona(generarDNI(), "Juan", 35, 'H',100.5, 1.80)
+    println(persona2.dni)
 }
