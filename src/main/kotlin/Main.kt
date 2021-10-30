0281287f9f1c17e33a34203af95a6c673746911f
Alejandro <amogalla@iesrafaelalberti.es>
2021-10-30 11:43:59 +0200

    Mejoradas algunas funciones.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 class Persona(val dni:String) {
     var nombre: String = ""
     var edad: Int = 0
@@ -14,17 +16,23 @@ class Persona(val dni:String) {
         const val INFRAPESO = -1
         const val PESO_IDEAL = 0
         const val SOBREPESO = 1
+        const val PESO_ERRONEO = 2
 
         fun infoPesoIdeal(persona: Persona){
             when (persona.calcularIMC()){
-                INFRAPESO -> println("Está por debajo de su peso")
-                PESO_IDEAL -> println("Está en su peso ideal")
-                SOBREPESO -> println("Tiene sobrepeso")
-                else -> println("El peso es erróneo")
+                INFRAPESO -> println(" está por debajo de su peso")
+                PESO_IDEAL -> println(" está en su peso ideal")
+                SOBREPESO -> println(" tiene sobrepeso")
+                PESO_ERRONEO -> println(" --> Imposible calcular el IMC. Introduzca la altura.")
             }
         }
-    }
 
+        fun infoMayoriaDeEdad(persona:Persona){
+            print(" tiene " + persona.edad + " años: ")
+            if(persona.esMayorDeEdad()) println("es mayor de edad.")
+            else println("es menor de edad.")
+        }
+    }
 
     constructor(dni:String, nombre:String, edad:Int, sexo:Char): this(dni){
         this.nombre = nombre
@@ -39,29 +47,28 @@ class Persona(val dni:String) {
 
 
     fun calcularIMC(): Int {
-        return when (peso/(altura*altura)){
+        return when (peso/(altura.pow(2))){
             in 1.0..20.0 -> INFRAPESO
             in 20.0..25.0 -> PESO_IDEAL
-            else -> SOBREPESO
+            in 25.0..Double.MAX_VALUE -> SOBREPESO
+            else -> PESO_ERRONEO
         }
     }
 
     fun esMayorDeEdad():Boolean= (edad >= 18)
 
     private fun comprobarSexo(sexo:Char): Char {
-        return when (sexo){
-            HOMBRE, MUJER -> sexo
+        return when (sexo.uppercase()[0]){  //Permitimos como válido tanto mayúsculas como minúsculas
+            HOMBRE, MUJER -> sexo.uppercase()[0]
             else -> HOMBRE
         }
     }
 
     override fun toString() = "Nombre = $nombre, edad = $edad, DNI = $dni, sexo = $sexo, peso = $peso, altura = $altura"
-
-
 }
 
 fun generaLetraDNI(numeroDNI:Int): Char {
-    val letra:String = "TRWAGMYFPDXBNJZSQVHLCKE"
+    val letra = "TRWAGMYFPDXBNJZSQVHLCKE"
     val resto:Int = numeroDNI%23
     return letra.get(resto)
 }
@@ -85,17 +92,16 @@ fun pedirNombre():String{
 
 fun pedirEdad():Int {
     print("Introduzca la edad: ")
-    return (readLine() ?: "0").toInt()
+    return try {
+        (readLine() ?: "0").toInt()
+    }catch(excep: NumberFormatException){0}
 }
 
-fun pedirPeso():Double {
-    print("Introduzca el peso: ")
-    return (readLine() ?: "0").toDouble()
-}
-
-fun pedirAltura():Double {
-    print("Introduzca la altura: ")
-    return (readLine() ?: "0").toDouble()
+fun pedirDouble(tipo:String):Double {
+    print("Introduzca $tipo: ")
+    return try {
+        (readLine() ?: "0").toDouble()
+    }catch(excep: NumberFormatException){0.0}
 }
 
 fun pedirSexo():Char {
@@ -103,16 +109,29 @@ fun pedirSexo():Char {
     return (readLine()?: "H")[0]
 }
 
-
-
 fun main() {
-    //val persona1 = Persona(generarDNI(), pedirNombre(), pedirEdad(), pedirSexo(), pedirPeso(), pedirAltura())
+    val persona1 = Persona(generarDNI(), pedirNombre(), pedirEdad(), pedirSexo(), pedirDouble("peso"), pedirDouble("altura"))
     val persona2 = Persona(generarDNI(), "Sandra", 27, 'M')
     val persona3 = Persona(generarDNI())
 
-   // Persona.infoPesoIdeal(persona1)
+    //INFO SOBRE EL PESO
+    print(persona1.nombre)
+    Persona.infoPesoIdeal(persona1)
+    print(persona2.nombre)
     Persona.infoPesoIdeal(persona2)
+    print(persona3.nombre)
     Persona.infoPesoIdeal(persona3)
 
+    //INFO SOBRE MAYORÍA DE EDAD
+    print(persona1.nombre)
+    Persona.infoMayoriaDeEdad(persona1)
+    print(persona2.nombre)
+    Persona.infoMayoriaDeEdad(persona2)
+    print(persona3.nombre)
+    Persona.infoMayoriaDeEdad(persona3)
 
+    //INFO COMPLETA DE CADA PERSONA
+    println(persona1.toString())
+    println(persona2.toString())
+    println(persona3.toString())
 }
